c46c2b89919b2267277e11007606aabd9e4ea4dc
JOHN <ghostcode541@gmail.com>
2025-04-13 17:04:42 -0300

    criptografia Monoalfabetica implementada

--- Encrypto.py
+++ Encrypto.py
@@ -1,11 +1,14 @@
+import random
  # Lê o conteúdo de um arquivo de texto
 try:
     with open('mensagem.txt', 'r', encoding='utf-8') as file:
-     mensagem = file.read().upper()  # Lê o arquivo e prepara a mensagem
+     texto = file.read().upper()  # Lê o arquivo e prepara a mensagem
 except FileNotFoundError:
      print("O arquivo 'mensagem.txt' não foi encontrado.")
 
+# tratamento da mensagem
 def Remover_acentos(texto):
+    
     try:
 
     # Dicionário com as substituições de caracteres acentuados
@@ -27,11 +30,19 @@ def Remover_acentos(texto):
     except Exception as e:
        print(f'Erro: {e}')
 
-mensagem = Remover_acentos(mensagem)
+mensagem = Remover_acentos(texto)
+while True:
+ try:
+  espaco = str(input("Deseja manter o espaçamento entre as letras na mensagem? [S/N] ")).upper()  
+  if espaco == "N" :
+   mensagem = mensagem.replace(' ','')
+   break
 
-espaco = str(input("Deseja manter o espaçamento entre as letras? [S/N] ")).upper()   
-if espaco == "N" :
-    mensagem = mensagem.replace(' ','')
+  if espaco == "S" :
+   break
+
+ except Exception as e:
+   print(f'Erro: {e}') 
 
 def Cesar():
     #  Cifra de César
@@ -46,7 +57,7 @@ def Cesar():
      
      while True:
       try:
-       crip_ou_descript = int(input('Digite 0 para criptografar e 1 se você quer descriptografar a mensagem: '))
+       crip_ou_descript = int(input(' [0]: criptografar [1]:descriptografar mensagem: '))
        if crip_ou_descript == 1 or crip_ou_descript == 0:
          break
        
@@ -57,7 +68,7 @@ def Cesar():
 
      for letra in mensagem:
 
-        if letra.isdigit() or letra in '.,!?=+-%@$':
+        if letra.isdigit() or letra in '.,!?=+-%@$;:&()':
 
             resultado += letra
             continue # skipa a iteração se for número
@@ -95,19 +106,149 @@ def Cesar():
     except Exception as e:
        print(f'Erro: {e}')
 
+def Monoalfabetica():
+ 
+ 
+#Cifra de Substituição (Cifra Monoalfabética)
+#Uma variação da Cifra de César, onde as letras são substituídas por outras aleatoriamente (não apenas com um deslocamento fixo).
+#Embora mais difícil de decifrar do que a Cifra de César, ainda é vulnerável a ataques como análise de frequência.
+
+ alfabeto= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+ letra_usada = []
+ alfabeto_copia = alfabeto.copy()
+ resultado = ''
+
+ while True:
+   try:
+    crip_ou_descript = int(input(' [0]: criptografar [1]: descriptografar mensagem: '))
+    if crip_ou_descript == 1 or crip_ou_descript == 0:
+      break
+      
+   except Exception as e:
+      print(f'Erro: {e}')
 
 
-# fazer um deslocamento "circular"
-# fazer as primeiras validações
+ if not crip_ou_descript: # cript_descript = False (criptografar)
+  
+  
+   while True:
+    try:
+     pergunta = int(input("[0]: alfabeto de substituição aleatória  [1]: alfabeto de substituição manual: "))
+     if pergunta == 1:
+        
+      # alfabeto de substiuiçao manual
+      for i, letra in enumerate(alfabeto):
+        nova_letra = str(input(f'Escreva o substituto da letra "{letra}" : ')).strip().upper()
+        while nova_letra == '' or len(nova_letra) != 1 or nova_letra in letra_usada:
+           print('><'*60)
+           print("Inválido! a nova letra deve ser apenas UM caractere e este não pode se repetir! ")
+           print('><'*60)
+           nova_letra = str(input(f'Escreva o substituto da letra "{letra}" : ')).strip().upper()
+          
+        letra_usada.append(nova_letra)
+        alfabeto[i] = nova_letra
 
-Cesar()
+      break
+    
+      # alfabeto de substiuição aleatória
+     if pergunta == 0:
+       # a letra nova não pode se repetir de novo no alfabeto
+       for i,letra in enumerate(alfabeto):
+        nova_letra = random.choice(alfabeto_copia)
+
+        while nova_letra == letra or nova_letra in letra_usada:
+          nova_letra = random.choice(alfabeto_copia)  
+    
+        letra_usada.append(nova_letra)
+        alfabeto[i] = nova_letra
+       
+       break
+
+    except Exception as e:
+      print(f'Erro: {e}')
+ 
+   print('=' *74)
+   print(f'este é o seu novo alfabeto de A-Z, guarde-o para conseguir desciptografá-lo depois :)')
+   print(f'Antes:  {alfabeto_copia}')
+   print(f'depois: {alfabeto}')
+   print("string:", ''.join(alfabeto))
+   print('=' *74) 
+  
+   # criptografia Monoalfabética
+   try:  
+    for letra in mensagem:
+
+     if letra.isdigit() or letra in '.,!?=+-%@$;:&()':
+         
+         resultado += letra
+         continue # skipa a iteração se for número ou símbolo
+   
+     if letra in alfabeto_copia:
+     
+      posicao_alfabeto = alfabeto_copia.index(letra)
+
+      letra = alfabeto[posicao_alfabeto]
+
+      resultado += letra
+   
+     else:
+       if letra == ' ':
+        resultado += letra
+
+   except Exception as e:
+    print(f'Erro: {e}')
 
 
-#def Monoalfabetica():
-    #Cifra de Substituição (Cifra Monoalfabética)
-    #Uma variação da Cifra de César, onde as letras são substituídas por outras aleatoriamente (não apenas com um deslocamento fixo).
-   #Embora mais difícil de decifrar do que a Cifra de César, ainda é vulnerável a ataques como análise de frequência.
+ else: # cript_descript = True (descriptografar)
+   
+   # Descriptografia
+   try:
+    while True:
+     print('=' * 60)
+     alfabeto = str(input('Informe o alfabeto que foi utilizado para criptografar a mensagem: ')).replace(' ','').upper()
+    
+     if len(alfabeto) == 26:
+       alfabeto = list(alfabeto)
+       break
+     
+     print('><' * 60)
+     print(f'Erro! o alfabeto brasileiro possui 26 letras! Você forneceu {len(alfabeto)} letras!')
+     print('><' * 60)
+    
+    for letra in mensagem:
 
+      if letra.isdigit() or letra in '.,!?=+-%@$;:&()':
+         
+         resultado += letra
+         continue # skipa a iteração se for número ou símbolo
+   
+      if letra in alfabeto:
+     
+       posicao_alfabeto = alfabeto.index(letra)
+
+       letra = alfabeto_copia[posicao_alfabeto]
+
+       resultado += letra
+
+      else:
+       if letra == ' ':
+        resultado += letra
+
+   except Exception as e:
+    print(f'Erro: {e}')
+
+ # Salvando o resultado em um arquivo de texto
+ with open("mensagem_resultado.txt", "w") as file:
+   file.write(resultado)  # Escreve a criptografia no arquivo de texto
+
+ print("O resultado foi salvo no arquivo 'mensagem_resultado.txt'.")
+
+       
+     
+Monoalfabetica()
+
+# gerar letra aleatoria, conferir se a letra gerada é diferente da letra original, adicionar cada letra aleaotria para a lista novo alfabeto
+# substituir cada letra da mensagem pelo novo alfabeto
 
 #def AES():
    #É um algoritmo de criptografia simétrica, o que significa que a mesma chave é usada tanto para criptografar quanto para descriptografar os dados.
